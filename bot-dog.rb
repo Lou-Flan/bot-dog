4d8418dd1a3362899ce6ac84ccbcdea51556d705
Louise <louise_flanagan@outlook.com>
2019-11-12 14:54:54 +1100

    Simple menu screen

---
+++ bot-dog.rb
@@ -0,0 +1,12 @@
+user_name = ARGV
+
+### Menu Screen ###
+def menu
+    puts "Hooman! What would you like to do?"
+    puts "Press 1 to pat BotDog"
+    puts "Press 2 to feed BotDog"
+    puts "Press 3 to play with BotDog"
+    puts "Press 000 to quit"
+end
+
+menu
