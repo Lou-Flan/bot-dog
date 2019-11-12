3401b2718d8f0b02d62f1b8ce52a9e1fb87a594c
Louise <louise_flanagan@outlook.com>
2019-11-12 15:09:25 +1100

    Added pat counter in method, using global variables

--- bot-dog.rb
+++ bot-dog.rb
@@ -9,4 +9,31 @@ def menu
     puts "Press 000 to quit"
 end
 
+### Counters ###
+def pat
+$pat_counter += 1
+    if $pat_counter == 3
+        puts 
+        puts "BotDog.... loves.... patss zzz zzz zzzzzz"
+        puts "ZZZ ZZZ ZZZ"
+        exit
+    elsif $pat_counter < 3
+        puts "Bork!"
+        puts "Good boi loves pats"
+        puts "<3"
+    end
+end
+
+
+
 menu
+
+selection = gets.chomp.to_i
+
+$pat_counter = 0
+$feed_counter = 0
+$play_counter = 0
+
+if selection == 1
+    pat
+end
