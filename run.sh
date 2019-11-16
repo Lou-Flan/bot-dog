85bb681b3327ccc6648b1c3148e320e18db29c0a
Louise <louise_flanagan@outlook.com>
2019-11-16 16:08:30 +1100

    Amended bash script to check operating system, if not running on mac the app will not run. Also added script to set volume at 50% in case user has volume on mute

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ruby bot-dog.rb Hooman
+if [ "$(uname)" == "Darwin" ]; then
+    osascript -e "set Volume 5"
+    ruby bot-dog.rb Hooman        
+else
+    echo "Thanks for downloading. Unfortunately BotDog is refusing to come out and play because you don't have a mac. Try again when you get one."
+fi
+
